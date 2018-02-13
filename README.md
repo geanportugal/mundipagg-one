6d90c76dbcec9a73fbea7e37575493368c50124d
Deepak Vashist <deepak@MacBook-Pro-de-Deepak.local>
2018-02-13 16:01:32 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# mundipagg-one-python
+
+### Installing with pip
+`pip install mundipagg_one_python`
+
+### Required Packages
+
+- `requests >= 2.0.0`
+- `enum34 >= 1.0.0`
+- `xmltodict >= 0.9.2`
+
+### Getting Started
+[Acesse a nossa página Wiki](https://github.com/mundipagg/mundipagg-one-python/wiki)
+
+## Work with us!
+
+Gostou da nossa SDK? Estamos sempre em busca de gente boa pra codar!
+
+Manda email (vagas@mundipagg.com) ou dá uma olhada nas [vagas](https://github.com/mundipagg/vagas) e vem conhecer a gente! :smile:
+
+Dev queremos você!
+
+Did you like our SDK? We're always looking for good coders!
+
+Send us an email (vagas@mundipagg.com) or take a look at our [jobs](https://github.com/mundipagg/vagas) page and come meet us! :smile:
+
+![We want you](https://raw.githubusercontent.com/mundipagg/vagas/master/we_want_you.jpg)
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'mundipaggOnePython'))
+
+setup(
+    name='mundipagg_one_python',
+    version='1.0.1',
+    package_dir={'mundipaggOnePython': 'mundipaggOnePython', 'data_contracts': 'mundipaggOnePython/data_contracts', 'enum_types': 'mundipaggOnePython/enum_types', 'resource_clients': 'mundipaggOnePython/resource_clients', 'transaction_report_file': 'mundipaggOnePython/transaction_report_file'},
+    packages=['mundipaggOnePython', 'data_contracts', 'enum_types', 'resource_clients', 'transaction_report_file'],
+    url='https://github.com/mundipagg/mundipagg-one-python',
+    license='Apache',
+    author='Newton Rocha',
+    author_email='nrocha@mundipagg.com',
+    description='Sdk for integration with mundipagg payment api',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    install_requires=['requests>=2.0.0', 'enum34>=1.0.0', 'xmltodict>=0.9.2'],
+    keywords=['mundipagg', 'rest', 'sdk', 'payments']
+)
